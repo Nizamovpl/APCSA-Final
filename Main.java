a5dcbfe0eea3320c6bff3cbccb75da81aee7fcd4
Percy Nizamov <Mysticgummibear@gmail.com>
2020-12-15 05:49:53 -0700

    Basic Structure

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main{
+    public void main(String[] args)throws IOException{
+        File file; 
+        Scanner scan; 
+
+        file = new File("save"); 
+        scan = new Scanner(file); 
+
+        //etc
+        Char char1 = new Char();
+        Char char2 = new Char();  
+        scan.close();
+
+    }
+
+}
